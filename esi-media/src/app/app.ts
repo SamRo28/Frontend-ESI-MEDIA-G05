9ce7392cba3257a12b16eb8b24595c6606c558c8
SamRo28 <Samuel.Rodero@alu.uclm.es>
2025-10-13 18:03:20 +0200

    Agrego página home y login

--- esi-media/src/app/app.ts
+++ esi-media/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('esi-media');
-    toggleDarkMode() {
+  
+  constructor(private router: Router) {}
+  
+  toggleDarkMode() {
     document.documentElement.classList.toggle('dark');
+  }
+  
+  navigateToHome() {
+    this.router.navigate(['/home']);
   }
 }
---
+++ esi-media/src/userService.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+    constructor(private client: HttpClient) {}
+
+    login(email: string, password: string): Observable<any> {
+        return this.client.post<string>(`http://localhost:8080/users/login`, { email, password }, { responseType: 'text' as 'json' })
+        
+    }
+
+}
